285a391fa4eb38b51821186e82d06e81ce1f4373
BCE provisioner <bce@lists.berkeley.edu>
2014-05-28 14:13:12 -0700

    B passed

--- challenges/01-Intro/B_data_and_variables.py
+++ challenges/01-Intro/B_data_and_variables.py
@@ -3,16 +3,19 @@
 # instructors
 
 ## what are our names?
-instructors = ''
+instructors = 'Dav' , 'Sean'
 
 # types
 ## Now define some other types of variables
 
 ## some_int should be an integer
+some_int = 1
 
 ## some_float should be a float
+some_float = 1.
 
 ## some_bool should be... what do you think?
+some_bool = True
 
 # redefining
 ## Here, I define a variable
@@ -20,6 +23,7 @@ my_lucky_number = 7
 
 ## But Dav's lucky number is 9 (and he wrote the test)
 ## Redefine the value "correctly" below
+my_lucky_number = 9
 
 ## Only the most recent variable assignment matters
 print "Dav's lucky number:", my_lucky_number
